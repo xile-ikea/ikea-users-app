855e2ced49a6b5f42de67cac56faef1a23921d38
MATHIAS ELIASSON <mattis.eliasson@gmail.com>
2024-09-19 16:13:04 +0200

    user create feature added

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -13,13 +13,44 @@ function App() {
     console.log(usersToJson)
     setUsers(usersToJson);
   };
+
+
+  const createUser = async (e) => {
+    e.preventDefault ()
+    const response = await fetch(`${hostUrl}api/users`,{
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({name: e.target.name.value, isAdmin: e.target.isAdmin.checked}),
+
+    });
+    const newUser = await response.json();
+
+      setUsers([...users, newUser]);
+    }
+
+
+
+
   
   useEffect(() => {
     fetchUsers();
   }, []);
 
   return (
+
     <>
+
+<h1>New User</h1>
+<form onSubmit={createUser}>
+    <label htmlFor="name">Name</label>
+    <input type="text" name="name" id="name" />
+    <label htmlFor="isAdmin">Is Admin</label>
+    <input type="checkbox" name="isAdmin"/>
+    <input type="submit" />
+</form>
+
       <h1>Users</h1>
       <table>
         <thead>
