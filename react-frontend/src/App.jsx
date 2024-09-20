4dee76bd8cdc494b655913acda50fe8c916427e5
MATHIAS ELIASSON <mattis.eliasson@gmail.com>
2024-09-20 08:51:51 +0200

    Done

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,84 +1,97 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState ([]);
+  const [users, setUsers] = useState([]);
   const hostUrl = import.meta.env.PROD
-  ? window.location.href
-  : "http://localhost:8080/";
+    ? window.location.href
+    : "http://localhost:8080/";
 
   const fetchUsers = async () => {
     const response = await fetch(`${hostUrl}api/users`);
-    const usersToJson = await response.json ();
-    console.log(usersToJson)
+    const usersToJson = await response.json();
+    console.log(usersToJson);
     setUsers(usersToJson);
   };
-
-
-  const createUser = async (e) => {
-    e.preventDefault ()
-    const response = await fetch(`${hostUrl}api/users`,{
-      method: 'POST',
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({name: e.target.name.value, isAdmin: e.target.isAdmin.checked}),
-
-    });
-    const newUser = await response.json();
-
-      setUsers([...users, newUser]);
-    }
-
-
-
-
-  
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  const updateUser = async (e) => {
+    const response = await fetch(`${hostUrl}api/users/${e.target.dataset.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ isAdmin: e.target.checked }),
+    });
+    await response.json();
+    await fetchUsers();
+  };
+
   const deleteUser = async (e) => {
     await fetch(`${hostUrl}api/users/${e.target.dataset.id}`, {
-    method: "DELETE",
-    headers: {
+      method: "DELETE",
+      headers: {
         "Content-type": "application/json",
-    },
+      },
     });
     await fetchUsers();
-}
+  }
+
+  const createUser = async (e) => {
+    e.preventDefault()
+    const response = await fetch(`${hostUrl}api/users`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ name: e.target.name.value, isAdmin: e.target.isAdmin.checked  }),
+    });
+    const newUser = await response.json();
+
+    setUsers([...users, newUser]);
+  }
+  
 
   return (
-
     <>
-
-<h1>New User</h1>
-<form onSubmit={createUser}>
-    <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" />
-    <label htmlFor="isAdmin">Is Admin</label>
-    <input type="checkbox" name="isAdmin"/>
-    <input type="submit" />
-</form>
-
+      <h1>New User</h1>
+      <form onSubmit={createUser}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="isAdmin">Is Admin</label>
+        <input type="checkbox" name="isAdmin"/>
+        <input type="submit" />
+      </form>
+      <br></br>
       <h1>Users</h1>
       <table>
         <thead>
           <tr>
             <th>Name</th>
             <th>Is Admin</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
           {users.map((user) => (
             <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.isAdmin.toString()}</td>
               <td>
-                <button data-id={user.id} onClick={deleteUser}style={{ backgroundColor: 'red' }}>Delete</button>
-                <button style={{ backgroundColor: 'yellow' }}>Update</button>
+                <p>{user.name}</p>
               </td>
-
+              <td>
+                <input
+                  data-id={user.id}
+                  type="checkbox"
+                  checked={user.isAdmin}
+                  onChange={updateUser}
+                />
+              </td>
+              <td>
+                <button data-id={user.id} onClick={deleteUser}>Delete</button>
+                <button data-id={user.id} onClick={updateUser}>Update</button>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -88,3 +101,4 @@ function App() {
 }
 
 export default App;
+
